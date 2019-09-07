6c94db0189502875de9393fc984e8a4b4a49d4f4
Evan Feder <evanmfeder@gmail.com>
2019-09-07 17:56:53 -0400

    Image setup

--- gangame/src/App.js
+++ gangame/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import Image from 'Image';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+
+  state = {
+    order: 0,
+  };
+
+  chooseOrder = () => {
+    this.setState(Math.random() > 0.5);
+  };
+
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <Image type={this.state.order ? "fake" : "real"}></Image>
+          <Image type={this.state.order ? "real" : "fake"}></Image>
+
+
+          <p>
+            Which one is the <b>fake</b> person?
+          </p>
+          
+  
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ gangame/src/Image.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+class Image extends React.Component {
+
+
+  chooseImage = () => {
+    if this.props.type = 
+  };
+
+  handleClick = () => {
+    
+  };
+
+
+  render() {
+    return (
+      <div className="Image">
+
+        <img src={chooseImage()} />
+
+      </div>
+    );
+  }
+  }
+  
+  export default Image;
+  
