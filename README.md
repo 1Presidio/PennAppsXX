cd79a046360f22a2fe67fb36b1d0cbc44bc57cf3
Kevin <kevin2kyoto@gmail.com>
2019-09-08 14:23:43 -0400

    final changes

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# PennAppsXX
-This project was for PennAppsXX. It is inspired by StyleGan from Nvidia Labs. [https://pennapps.com/]
+# PennApps19
+This project is for PennApps19 [https://pennapps.com]. It is inspired by StyleGan from Nvidia Labs. [https://github.com/NVlabs/stylegan]
 
 ## The GAN Game
 
 GANs (Generative Adversarial Networks) are a type of neural network which specialize is creating false images. Can you guess which image is real and which is simply a GAN-created fake? Play the GAN Game.
+
+## Try It Out!
+https://1presidio.github.io/PennApps19/
--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import Image from './Image';
 import './App.css';
 
-
-
-
 class App extends React.Component {
-
   state = {
     order: false,
     result: "",
@@ -19,15 +15,15 @@ class App extends React.Component {
   };
 
   chooseOrder = () => {
-    this.setState({order: Math.random() > 0.5});
+    this.setState({ order: Math.random() > 0.5 });
   };
 
   displayResult = (num) => {
     if (num == this.state.order) {
-      this.setState({result: "Correct!"});
+      this.setState({ result: "Correct!" });
     }
     else {
-      this.setState({result: "Not correct!"});
+      this.setState({ result: "Not correct!" });
     }
   };
 
@@ -35,10 +31,10 @@ class App extends React.Component {
     let numReal = Math.floor(Math.random() * 103) + 7;
     let numFake = Math.floor(Math.random() * 250);
     if (this.state.order) {
-      this.setState({img1: "./real_images/res" + String(numReal)+ ".png", img2: "./fake_images/res" + String(numFake)+ ".jpg"});
+      this.setState({ img1: "./real_images/res" + String(numReal) + ".png", img2: "./fake_images/res" + String(numFake) + ".jpg" });
     }
     else {
-      this.setState({img1: "./fake_images/res" + String(numFake)+ ".jpg", img2: "./real_images/res" + String(numReal)+ ".png"});
+      this.setState({ img1: "./fake_images/res" + String(numFake) + ".jpg", img2: "./real_images/res" + String(numReal) + ".png" });
     }
   };
 
@@ -47,29 +43,29 @@ class App extends React.Component {
     this.chooseImage();
   };
 
-
-  render () {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1> The GAN Game </h1>
-
           <p>
             {this.state.result}
           </p>
-          
           <Image img={this.state.img1} num="0" displayResult={this.displayResult} reset={this.reset}></Image>
           <Image img={this.state.img2} num="1" displayResult={this.displayResult} reset={this.reset}></Image>
-
           <p>
             Which one is the <b>fake</b> person?
           </p>
-          
+          <p>
+            Made by Kevin Hong (1presidio), Evan Feder (EvanFeder), and Daniel Lee (dj59205).
+          </p>
+          <p>
+            https://www.1presidio.github.io/
+          </p>
         </header>
       </div>
     );
   }
-  
 }
 
 export default App;
