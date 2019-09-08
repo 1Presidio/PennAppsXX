84faa7db729192db5d76ea847d3f7ddf622e50dd
Evan Feder <evanmfeder@gmail.com>
2019-09-08 11:32:48 -0400

    Text updates

--- gangame/src/App.js
+++ gangame/src/App.js
@@ -52,6 +52,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h1> The GAN Game </h1>
 
           <p>
             {this.state.result}
