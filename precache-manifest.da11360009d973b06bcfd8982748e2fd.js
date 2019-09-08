a1c6b6b09fb7e68e2544f73bbaeb3f83349d8ebb
Kevin <kevin2kyoto@gmail.com>
2019-09-08 14:06:50 -0400

    Updates

---
+++ precache-manifest.da11360009d973b06bcfd8982748e2fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06888517986f9a6162dcbe3f69ad1b4",
+    "url": "/PennApps19/index.html"
+  },
+  {
+    "revision": "23e6e8bfd29e26a942bb",
+    "url": "/PennApps19/static/css/main.b311dd2c.chunk.css"
+  },
+  {
+    "revision": "5ffab4dd2851bff0caa1",
+    "url": "/PennApps19/static/js/2.c427fc7e.chunk.js"
+  },
+  {
+    "revision": "23e6e8bfd29e26a942bb",
+    "url": "/PennApps19/static/js/main.cae145aa.chunk.js"
+  },
+  {
+    "revision": "65fc1d8959aff13e3dba",
+    "url": "/PennApps19/static/js/runtime~main.cbbd914a.js"
+  }
+]);
